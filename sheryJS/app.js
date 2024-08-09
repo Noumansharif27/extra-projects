44cd25106266662aea29a6cd0da306c38e53c223
Nouman Sharif <noumansharifgul27@gmail.com>
2024-08-09 15:37:01 +0500

    working on sheryJS

--- sheryJS/app.js
+++ sheryJS/app.js
@@ -1,8 +1,24 @@
-let img = document.querySelector(".img");
-shery.imageEffect(".img", {
-  style: 5,
-  config: true,
-  // sliderStyle: (setScroll) => {
+// let img = document.querySelector(".img");
+// shery.imageEffect(".img", {
+//   style: 5,
+//   config: true,
+//   // sliderStyle: (setScroll) => {
 
-  // }
+//   // }
+// });
+
+// Shery.mouseFollower({
+//   //Parameters are optional.
+//   skew: true,
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+// });
+
+Shery.imageEffect(".img", {
+  style: 2, //Select Style
+  debug: true, // Debug Panel
+  config: {
+    /* Config made from debug panel */
+  },
+  preset: "./presets/wigglewobble.json",
 });
